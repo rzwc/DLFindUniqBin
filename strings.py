ddc123f72b417c1eb9daa97e9a1a49ff0093eaf8
Richard Chow <richard.chow@ucalgary.ca>
2020-06-09 16:30:07 -0600

    adding back test files

--- strings.py
+++ strings.py
@@ -1,54 +1,73 @@
+# Program imitating Linux strings command
 import sys
 
+# minimum length of string
 MINLEN = 4
 
+# is printable strings function, takes character
+# returns true if integer representing Unicode character is between ' ' and - 
+# or is tab character
 def isprint(b):
 	return b >= ord(' ') and b <= ord('~') or b == ord('\t')
 
+# output printable strings 
 def strings(filename):
 	try:
+        # open binary file to read
 		f = open(filename, 'rb')
 		data = f.read()
+    # print usage format if IOError
 	except IOError as e:
 		print(f'{filename}: {e.strerror}', file=sys.stderr)
 		sys.exit()
-
-	###
-	# print(len(data), type(data), isprint(data[0]), isprint(data[1]))
-	###
-	# user/share/dict/words english words/spell checker
-	# diff of strings and stringlish for types of patterns to reject
-	# script to compute diff of strings and stringlish and take out
-	# compare sets, iterate through one 
 	
-	
-	x = 0
+    # initialize variable to track index, and the last character of the string if found 
+	index = 0
 	last = 0
-	while x < len(data) - 4: 
-		last = x
-		if isprint(data[x]) == True:
-			start = x
+    
+    # keep loop inside length of data
+    # if string is found past len(data) - MINLEN, string will be less than 4 characters
+	while index < len(data) - MINLEN: 
+        # set last character checked as current index 
+		last = index
+        
+        # if current indexed character is a printable character 
+		if isprint(data[index]) == True:
+            # start index is the current index, track head of the string
+			start = index
+            # counter to count to MINLEN 
 			counter = 0
-			bool = 0
+            # boolean to ascertain whether string is printable or not 
+			canprint = 0
+            # number of printable characters in 4 characters
 			length = 0
+            
+            # loop through minimum length to check whether there are 4 consecutive printable characters 
 			while counter < MINLEN:
-				if isprint(data[x]) == True:
-					length = length + 1
-				x = x + 1
-				counter = counter + 1
-			if length >= MINLEN:
-				bool = 1
+                # if current indexed character is printable, increase length by 1
+				if isprint(data[index]) == True:
+					length += 1
+                # move to next character
+				index += 1
+                #increase counter
+				counter += 1
+            
+            # if there are 4 consecutive printable characters, canprint is true
+			if length == MINLEN:
+				canprint = 1
 			
-			x = start
+            # set index back to head of the string 
+			index = start
 			
-			while bool == 1:
-				print(chr(data[x]), end = "")
-				if isprint(data[x+1]) == False:
-					last = x 
-					bool = 0
+            # 
+			while canprint == 1:
+				print(chr(data[index]), end = "")
+				if isprint(data[index+1]) == False:
+					last = index 
+					canprint = 0
 					print()
-				x = x + 1
-		x = last + 1
+				index += 1
+		index = last + 1
 	f.close()
 	
 if __name__ == '__main__':
